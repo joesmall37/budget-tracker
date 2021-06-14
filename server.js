1c276fee1a81f73f01b35da30150f0f73c1354fe
Joe Small <small.joseph@outlook.com>
2021-06-14 13:51:10 -0400

    connected to mongodb atlas

--- server.js
+++ server.js
@@ -15,10 +15,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+
+// set up mongo database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+
+})
 
 // routes
 app.use(require("./routes/api.js"));
