7fd20fc0e051e25aed7bbe16bc1452a2bb58567f
Joe Small <small.joseph@outlook.com>
2021-07-20 11:49:37 -0400

    cleaned code

--- server.js
+++ server.js
@@ -1,39 +1,31 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(logger("dev"));
-
+// set up middleware
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
 
 // set up mongo database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI, {
+var MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost/budget";
+mongoose.connect(MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 
 })
-
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port: ${PORT}`);
 });
